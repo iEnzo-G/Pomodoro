640ff69c9023c2c9714c1a2502f9db0dd7183234
Enzo Gammino <96486074+iEnzo-G@users.noreply.github.com>
2023-04-19 11:18:16 +0200

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Enzo Gammino on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
